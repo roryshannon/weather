2b82e71cc5554ec2d58f478173e6d1c9474e0a36
Rory Shannon <58435485+roryshannon@users.noreply.github.com>
2023-03-12 20:47:30 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,21 @@
 #well that was kinda easy i guess? need to check if the key resets tomorrow though
 import requests
 import sys
+from datetime import date
+ 
+
 
 import json
   
 #oh also regardless I need to make a date var
 place = "London"
-response = requests.request("GET", f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}/2023-02-28/2023-02-28?unitGroup=us&elements=name%2Ctemp%2Cprecip%2Cwindspeed%2Cpressure%2Csunrise%2Cicon&include=current&key=E6NN76EW4SM8X265MB757RJAU&contentType=json")
+date = date.today()
+
+place = input("Type a location!")
+
+response = requests.request("GET", f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}/{date}/{date}?unitGroup=us&elements=name%2Ctemp%2Cprecip%2Cwindspeed%2Cpressure%2Csunrise%2Cicon&include=current&key=E6NN76EW4SM8X265MB757RJAU&contentType=json")
 if response.status_code!=200:
-  print('Unexpected Status code: ', response.status_code)
+  print('sorry, could not find weather for', place)
   sys.exit()  
 
 
@@ -20,4 +27,4 @@ jsonData = response.json()
 #fucking farenheit so like -32 x 5/9?
 wrongTemp = jsonData['days'][0]['temp']
 temp = round(((wrongTemp-32) * (5/9)), 1)              
-print(f"the temperature in {place} is " + str(temp))
+print(f"the temperature in {place} today is " + str(temp))
